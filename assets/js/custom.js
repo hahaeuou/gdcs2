0f520230f98fc48127a36168289d4fc1448d3e58
chucorate <matias.olate12@gmail.com>
2026-03-08 02:27:20 -0300

    Add new features to homepage

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,19 @@
 // Put your custom JS code here
+
+const DAYS_BEFORE_NEW_TAG_EXPIRE = 28;
+
+// Checks if a guide has been visited before and its creation date,
+// in order to whether or not display the "New" badge at the "Latest Guides" homepage section
+document.querySelectorAll(".new-badge").forEach(badge => {
+  const id = "isVisited_" + badge.dataset.guide;
+
+  const guideDate = new Date(badge.dataset.date);
+  const currentDate = new Date();
+  const daysSincePublish = (currentDate - guideDate) / (1000 * 60 * 60 * 24);
+
+  if (!localStorage.getItem(id) && (daysSincePublish <= DAYS_BEFORE_NEW_TAG_EXPIRE)) {
+    badge.style.display = "block";
+  } else {
+    badge.remove();
+  }
+});
